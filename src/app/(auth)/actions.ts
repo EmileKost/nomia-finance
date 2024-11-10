c38c92de4d63fe52bdbc6882f9733f31ea845669
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2024-11-10 11:19:26 +0100

    feat: actions

---
+++ src/app/(auth)/actions.ts
@@ -0,0 +1,13 @@
+"use server";
+
+export const login = () => {
+	console.log("Logging in");
+};
+
+export const register = () => {
+	console.log("Registering");
+};
+
+export const logout = () => {
+	console.log("Logging out");
+};
