f4e2c0fb138fe7b3be9c61e172110c62096055b3
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2024-11-10 11:34:32 +0100

    chore: setting up supabase

--- src/app/(auth)/actions.ts
+++ src/app/(auth)/actions.ts
@@ -1,5 +1,9 @@
 "use server";
 
+import createClient from "@/utils/supabase/server";
+
+const supabase = await createClient();
+
 export const login = () => {
 	console.log("Logging in");
 };
@@ -10,4 +14,10 @@ export const register = () => {
 
 export const logout = () => {
 	console.log("Logging out");
+};
+
+export const getUsersData = async () => {
+	const { data: users } = await supabase.from("users").select();
+
+	return users;
 };
